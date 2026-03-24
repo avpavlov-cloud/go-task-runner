47105016b6d587896a84dc07baec30892470b493
aleksey <av.pavlow@gmail.com>
2026-03-24 19:45:32 +0300

    feat: Добавить профилировщик в проект - Добавил профилировщик в проект

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
+	_ "net/http/pprof" // Магия: регистрирует обработчики pprof
 	"taskrunner/internal/runner"
 	"time"
 )
 
 func main() {
+	// Запускаем pprof сервер в фоне
+	go func() {
+		fmt.Println("Pprof доступен на: http://localhost:6060/debug/pprof/")
+		if err := http.ListenAndServe("localhost:6060", nil); err != nil {
+			fmt.Printf("Ошибка pprof сервера: %v\n", err)
+		}
+	}()
+
 	// Контекст закроется сам через 3 секунды
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Second) //Увеличил для профилировщика с 3 до 300
 	defer cancel()
 
 	sched := runner.NewScheduler(10)
@@ -18,14 +28,14 @@ func main() {
 	sched.Start(ctx, 3)
 
 	// Накидываем 5 задач
-	for i := 1; i <= 5; i++ {
+	for i := 1; i <= 1000; i++ {
 		// Берем объект из пула (утверждаем тип через .(type))
 		task := runner.TaskPool.Get().(*runner.SimpleTask)
 
 		// 2. Инициализируем новыми данными
 		task.ID = fmt.Sprintf("%d", i)
-		
-		sched.Submit(&runner.SimpleTask{ID: fmt.Sprintf("%d", i)})
+
+		sched.Submit(task)
 	}
 
 	sched.Wait()
